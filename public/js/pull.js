0638d55d846b17ebf26373b75891ef58761c9c51
Kevin Wang <kevmo314@gmail.com>
2021-03-24 03:00:33 -0400

    update to use rtdb

--- public/js/pull.js
+++ public/js/pull.js
@@ -11,16 +11,19 @@ function loadScript(url, callback) {
 function addLocationListener(callback) {
   var key = new URLSearchParams(window.location.search).get("key");
   return firebase
-    .firestore()
-    .collection("pullables")
+    .database()
+    .ref()
+    .child("pullables")
+    .child(key)
+    .child("location")
     .doc(`${key}:location`)
-    .onSnapshot(function (doc) {
-      callback(doc.data());
+    .on("value", function (snapshot) {
+      callback(snapshot.val());
     });
 }
 
 loadScript("/__/firebase/8.2.9/firebase-app.js", function () {
-  loadScript("/__/firebase/8.2.9/firebase-firestore.js", function () {
+  loadScript("/__/firebase/8.2.9/firebase-database.js", function () {
     loadScript("/__/firebase/init.js", function () {
       onReady();
     });
