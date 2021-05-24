3627a3e90a3f8697453c09fc9b7fa1e9483abd2e
Kevin Wang <kevmo314@gmail.com>
2021-05-23 23:03:12 -0400

    api

--- api/src/index.ts
+++ api/src/index.ts
@@ -11,62 +11,90 @@ export function addLocationListener(
   pullKey: string,
   callback: (location: Location) => void
 ) {
-  return getRef(pullKey, "location").on("value", (snapshot) => {
-    callback(snapshot.val());
-  });
+  return forPullKey(pullKey).addLocationListener(callback);
 }
 
 export function addSpeedListener(
   pullKey: string,
   callback: (speed: MetersPerSecond) => void
 ) {
-  return getRef(pullKey, "speed").on("value", (snapshot) => {
-    callback(snapshot.val());
-  });
+  return forPullKey(pullKey).addSpeedListener(callback);
 }
 
 export function addHeadingListener(
   pullKey: string,
   callback: (heading: Degrees) => void
 ) {
-  return getRef(pullKey, "heading").on("value", (snapshot) => {
-    callback(snapshot.val());
-  });
+  return forPullKey(pullKey).addHeadingListener(callback);
 }
 
 export function addAltitudeListener(
   pullKey: string,
   callback: (altitude: Meters) => void
 ) {
-  return getRef(pullKey, "altitude").on("value", (snapshot) => {
-    callback(snapshot.val());
-  });
+  return forPullKey(pullKey).addAltitudeListener(callback);
 }
 
-/**
- * This listener detects changes in the active session id. When a streamer
- * goes online, a new session id is created and this listener is triggered.
- * null sessionId's indicate the streamer is offline. Note that it is
- * possible for two sequential non-null sessionIds to be sent.
- * @param pullKey
- * @param callback
- * @returns
- */
 export function addSessionIdListener(
   pullKey: string,
   callback: (sessionId: UUID) => void
 ) {
-  return getRef(pullKey, "sessionId").on("value", (snapshot) => {
-    callback(snapshot.val());
-  });
+  return forPullKey(pullKey).addSessionIdListener(callback);
+}
+
+/** Creates a listener source for a streamer's private data with a pull key. */
+export function forPullKey(pullKey: string) {
+  const ref = getDb().ref().child("pullables").child(pullKey);
+  return {
+    addLocationListener(callback: (location: Location) => void) {
+      return ref.child("location").on("value", (snapshot) => {
+        callback(snapshot.val());
+      });
+    },
+    addSpeedListener(callback: (speed: MetersPerSecond) => void) {
+      return ref.child("speed").on("value", (snapshot) => {
+        callback(snapshot.val());
+      });
+    },
+    addHeadingListener(callback: (heading: Degrees) => void) {
+      return ref.child("heading").on("value", (snapshot) => {
+        callback(snapshot.val());
+      });
+    },
+    addAltitudeListener(callback: (altitude: Meters) => void) {
+      return ref.child("altitude").on("value", (snapshot) => {
+        callback(snapshot.val());
+      });
+    },
+    /**
+     * This listener detects changes in the active session id. When a streamer
+     * goes online, a new session id is created and this listener is triggered.
+     * null sessionId's indicate the streamer is offline. Note that it is
+     * possible for two sequential non-null sessionIds to be sent.
+     */
+    addSessionIdListener(callback: (sessionId: UUID) => void) {
+      return ref.child("sessionId").on("value", (snapshot) => {
+        callback(snapshot.val());
+      });
+    },
+  };
+}
+
+/** Creates a listener source for a streamer's public data. */
+export function forStreamer(provider: "twitch", userId: string) {
+  const ref = getDb().ref().child("streamers").child(`${provider}:${userId}`);
+  return {
+    addLocationListener(callback: (location: Location | null) => void) {
+      return ref.child("location").on("value", (snapshot) => {
+        callback(snapshot.val());
+      });
+    },
+  };
 }
 
 let app: firebase.app.App | null = null;
 
-function getRef(
-  key: string,
-  type: "location" | "speed" | "heading" | "altitude" | "sessionId"
-) {
+function getDb() {
   if (!app) {
     app = firebase.initializeApp(
       {
@@ -78,5 +106,5 @@ function getRef(
       "rtirl-api"
     );
   }
-  return app.database().ref().child("pullables").child(key).child(type);
+  return app.database();
 }
