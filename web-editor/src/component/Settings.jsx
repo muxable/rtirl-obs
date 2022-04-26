c1a324590af6608594336f05eb4b0b3336667bb5
ChuangWei Ma <chungwwei223@gmail.com>
2022-04-26 17:15:15 -0400

    chore: removed unused

--- web-editor/src/component/Settings.jsx
+++ web-editor/src/component/Settings.jsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { useState } from 'react';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
