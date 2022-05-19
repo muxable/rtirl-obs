22a0707f8ee803ddb3601ca6fdce1a55f7a8656a
ChuangWei Ma <chungwwei223@gmail.com>
2022-05-18 20:40:19 -0400

    fix: using iframe instead of google library

--- web-editor/src/component/GoogleMapContainer.jsx
+++ web-editor/src/component/GoogleMapContainer.jsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { GoogleMap, useJsApiLoader, } from '@react-google-maps/api';
 import { useState, useCallback, useRef, useEffect } from 'react';
+import { mapboxMapStyleJsonCache } from "./RightPanel";
         
 
 const containerStyle = {
@@ -10,73 +11,21 @@ const containerStyle = {
 };
 
 
-
-
 export const GoogleMapContainer = ({ mapStyle, apiKey }) => {
   
+  const pullKey = "justANeededHardCodedKey";
   mapStyle = JSON.parse(mapStyle) ?? {};
+  const styleB64 = encodeURIComponent(window.btoa(JSON.stringify(mapStyle)));
+  const uri = `https://overlays.rtirl.com/googlemaps.html?key=${pullKey}&api_key=${apiKey}&style=${styleB64}`;
 
-  const center = {
-    lat: 40,
-    lng: -100
-  }
-  
-  
-  const { isLoaded, loadError } = useJsApiLoader({
-    id: `google-map-script`,
-    googleMapsApiKey: apiKey,
-  })
-
-  console.log(`google map api loaded: ${isLoaded}`);
-
-  const [map, setMap] = useState(null)
-
-  const onLoad = useCallback((map) => {
-    setMap(map)
-  }, [])
-
-  const onUnmount = useCallback((map) => {
-    setMap(null)
-  }, [])  
-
-  const onZoomChanged = () => {
-    // if (map !== undefined && map !== null) {
-    //   console.log(`zoom changed: ${map['zoom']}`);
-    //   setGoogleMapViewState({ latitude: map['center']['lat'], longitude: map['center']['lng'], zoom: map['zoom']})
-    // }
-  }
-
-  const onCenterChanged = () => {
-    // if (map !== undefined && map !== null) {
-    //   console.log(`center changed: ${map['center']}`);
-    //   setGoogleMapViewState({ latitude: map['center']['lat'], longitude: map['center']['lng'], zoom: map['zoom']})
-    // }
-  }
-
-  const onMapTypeIdChanged = () => {
-    console.log("map type changed");
-  }
-  
-  // if (loadError === undefined) {
-  //   return <div>Map cannot be displayed: {loadError.message}</div>
-  // }
-
-  return isLoaded ? (
-      <GoogleMap
-        options={{
-          styles: mapStyle,
-        }}
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={3}
-        onLoad={onLoad}
-        onUnmount={onUnmount}
-        onZoomChanged={onZoomChanged}
-        onCenterChanged={onCenterChanged}
-        onMapTypeIdChanged={onMapTypeIdChanged}
-      >
-        { /* Child components, such as markers, info windows, etc. */ }
-        <></>
-      </GoogleMap>
-  ) : <></>
+  return (
+    <>
+      <iframe 
+        style={containerStyle}
+        title="gmaps" 
+        frameBorder={0} 
+        src={uri}>
+      </iframe>`
+    </>
+  );
 }
