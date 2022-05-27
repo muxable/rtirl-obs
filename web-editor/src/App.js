7d27c6b134fcac4575abf6320fc2905f638e6f5d
ChuangWei Ma <chungwwei223@gmail.com>
2022-05-27 14:20:27 -0400

    chore: commentout unused

--- web-editor/src/App.js
+++ web-editor/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { EditorAppbar } from "./component/Appbar";
 import Stack from "@mui/material/Stack";
-import { EditorScreen } from "./screen/EditorScreen";
+// import { EditorScreen } from "./screen/EditorScreen";
 import { useState } from "react";
 import { SimpleTextEditScreen } from "./screen/SimpleTextEditorScreen";
 
