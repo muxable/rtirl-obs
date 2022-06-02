7c0f9bfc58c5ca60f04653bffaa048be8ab9182c
ChuangWei Ma <chungwwei223@gmail.com>
2022-06-01 21:17:34 -0400

    feat: heading-overlay

---
+++ web-editor/src/screen/HeadingEditor.jsx
@@ -0,0 +1,138 @@
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import {
+  Box,
+  Grid,
+  IconButton,
+  InputAdornment,
+  TextField,
+} from "@mui/material";
+import Typography from "@mui/material/Typography";
+import * as React from "react";
+import { useState } from "react";
+import CountryPicker from "../component/CountryPicker";
+import ExclusiveToggle from "../component/ExclusiveToggle";
+import PullKeyInput from "../component/PullKeyInput";
+import TextOverlayPreview from "../component/TextOverlayPreview";
+import { TextSettings } from "../component/TextSettings";
+
+
+const headingOptions = [
+  { name: "deg\u00B0", value: "deg" },
+  { name: "NSEW", value: "nsew" },
+];
+
+function HeadingEditor(props) {
+  const [pullKey, setPullKey] = useState({ value: "", valid: false });
+  const [units, setUnits] = useState("mph");
+	const [lang, setLang] = useState("en");
+  const [textDivCSS, setTextDivCSS] = useState({
+    textColor: "#94fe32",
+    fontFamily: "sans-serif",
+    rotation: 0,
+    fontSize: 30,
+    isBold: false,
+    isItalic: false,
+    opacity: 100,
+    backgroundColor: "#000000",
+    borderColor: "#ffffff",
+    borderWidth: 0,
+    padding: 0,
+    border_top_left_radius: 0,
+    border_top_right_radius: 0,
+    border_bottom_left_radius: 0,
+    border_bottom_right_radius: 0,
+    textAlign: "left",
+  });
+  const url = `https://overlays.rtirl.com/heading/${units}.html?key=${pullKey.value}`;
+
+	const headingCountries = [
+		"en",
+		"es",
+		"sv",
+		"tr"
+	]
+
+  const exportModule = (
+    <Box
+      style={{
+        marginTop: "8px",
+        padding: "8px",
+      }}
+      border={1}
+      borderColor="primary.border"
+      backgroundColor="primary.main"
+    >
+      <aside>
+        <h2> Speed Overlay URL </h2>
+        {pullKey.valid ? (
+          <TextField
+            readOnly
+            value={url}
+            fullWidth
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <IconButton
+                    onClick={() => {
+                      navigator.clipboard.writeText(url);
+                    }}
+                  >
+                    <ContentCopyIcon />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          ></TextField>
+        ) : (
+          <Typography color="inherit">
+            Pull key is required for a generic overlay URL
+          </Typography>
+        )}
+      </aside>
+    </Box>
+  );
+  return (
+    <Grid container columns={{ xs: 1, md: 12 }} direction="row">
+      <Grid item xs={1} md={2.5}>
+        <Box
+          style={{
+            padding: "16px",
+            height: "100%",
+          }}
+          borderRight={1}
+          borderBottom={1}
+          borderColor="primary.border"
+          backgroundColor="primary.main"
+          textAlign="left"
+        >
+          <Typography variant="h6" component="div">
+            Settings
+          </Typography>
+          <PullKeyInput pullKey={pullKey} onKeyChange={setPullKey} />
+          <ExclusiveToggle
+            name="Format"
+            selectedOption={units}
+            options={headingOptions}
+            onOptionChange={setUnits}
+          />
+					<Typography >
+						Language
+          </Typography>
+					<CountryPicker lang={lang} setLang={setLang} countries={headingCountries} />
+          <TextSettings textDivCSS={textDivCSS} setTextDivCSS={setTextDivCSS} />
+        </Box>
+      </Grid>
+      <Grid item xs={1} md={9.5} lg={12}>
+        <Box padding={1} paddingBottom={0}>
+          <TextOverlayPreview
+            text={units === "nsew" ? "NW": `1000\u00B0`}
+            textDivCSS={textDivCSS}
+          />
+          {exportModule}
+        </Box>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default HeadingEditor;
