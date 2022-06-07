d9add3c4e4424330f5f185ed10808449dd49e1e3
ChuangWei Ma <chungwwei223@gmail.com>
2022-06-07 16:49:17 -0400

    feat: reverse geocoding text

--- web-editor/src/screen/NeighborhoodEditor.jsx
+++ web-editor/src/screen/NeighborhoodEditor.jsx
@@ -13,12 +13,75 @@ import useStyle from "../hooks/useStyle";
 
 function NeighborhoodEditor({ pullKey, onPullKeyChange }) {
   const [textDivCSS, setTextDivCSS] = useStyle();
-  const [selected, setSelected] = useState(["place", "postcode", "address"]);
+  const [selected, setSelected] = useState([
+    "neighborhood",
+    "place",
+  ]);
   const [lang, setLang] = useState("en");
 
-  // const coordinates = [-73.990593, 40.740121];
+  const [text, setText] = useState("Williamsburg, Brooklyn, NY");
 
-  useEffect(() => {}, [selected, lang]);
+  const formatURL = () => {
+    var base = `https://overlays.rtirl.com/neighborhood.html?key=${pullKey.value}&format=`;
+    var formatStr = "";
+    const mp = {
+      neighborhood: "${data.neighborhood ? data.neighborhood.text + ' ' : ''}",
+      region: "${data.region ? data.region.text + ' ' : ''}",
+      country: "${data.country ? data.country.text + ' ' : ''}",
+      place: "${data.place ? data.place.text + ' ' : ''}",
+      postcode: "${data.postcode ? data.postcode.text + ' ' : ''}",
+      district: "${data.district ? data.district.text + ' ' : ''}",
+      locality: "${data.locality ? data.locality.text + ' ' : ''}",
+      poi: "${data.poi ? data.poi.text + ' ' : ''}",
+      address: "${data.address ? data.address.text + ' ' : ''}",
+    };
+    for (var param of selected) {
+      formatStr += encodeURIComponent(mp[param]);
+    }
+    return base + formatStr;
+  };
+
+  useEffect(() => {
+    const coordinates = [-73.990593, 40.740121];
+    const lng = coordinates[0];
+    const lat = coordinates[1];
+    const context = {};
+    fetch(
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?access_token=pk.eyJ1Ijoia2V2bW8zMTQiLCJhIjoiY2w0MW1qaTh3MG80dzNjcXRndmJ0a2JieiJ9.Y_xABmAqvD-qZeed8MabxQ&language=${lang}`
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        for (var param of [
+          "country",
+          "region",
+          "postcode",
+          "district",
+          "place",
+          "locality",
+          "neighborhood",
+          "address",
+          "poi",
+        ]) {
+          context[param] = res.features.find((feature) =>
+            feature.place_type.includes(param)
+          );
+        }
+        const ret = [];
+        for (var param of selected) {
+          if (context[param]) {
+            if (param === "country") {
+              ret.push((context[param]["properties"]["short_code"]).toUpperCase())
+            } else {
+              ret.push(context[param]["text"]);
+            }
+          }
+        }
+
+        setText(ret.join(", "));
+      })
+      .catch((err) => console.log(err));
+  }, [selected, lang, setText]);
 
   const fullSet = [
     "address",
@@ -88,6 +151,7 @@ function NeighborhoodEditor({ pullKey, onPullKeyChange }) {
           </Typography>
           <PullKeyInput pullKey={pullKey} onKeyChange={onPullKeyChange} />
           {/* Select Types */}
+          <Typography> Format, the order matters </Typography>
           <MultipleSelect
             selected={selected}
             setSelected={setSelected}
@@ -102,11 +166,12 @@ function NeighborhoodEditor({ pullKey, onPullKeyChange }) {
       </Grid>
       <Grid item xs={1} md={9.5} lg={12}>
         <Box padding={1} paddingBottom={0}>
-          <TextOverlayPreview text={`3`} textDivCSS={textDivCSS} />
+          <TextOverlayPreview text={text} textDivCSS={textDivCSS} />
           <OverlayExportPanel
-            overlayDescription="Distance Overlay URL"
+            overlayDescription="Neighborhood Overlay URL"
             isExportable={pullKey.valid}
             streamElementExportable={false}
+            url={formatURL()}
           />
         </Box>
       </Grid>
