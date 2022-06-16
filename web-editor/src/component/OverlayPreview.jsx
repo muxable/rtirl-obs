2b1f6dfc311b8a04799c470ad243ee4cfa8b87cb
Kevin Wang <kevmo314@gmail.com>
2022-06-15 20:14:23 -0400

    feat: add a section for community overlays

--- web-editor/src/component/OverlayPreview.jsx
+++ web-editor/src/component/OverlayPreview.jsx
@@ -5,31 +5,43 @@ import Typography from "@mui/material/Typography";
 import * as React from "react";
 import { Link } from "react-router-dom";
 
-export const OverlayPreview = ({ name, route, image }) => {
+export const OverlayPreview = ({ name, route, href, image }) => {
+  const content = (
+    <Card
+      square={true}
+      sx={{
+        ":hover": {
+          boxShadow: 20,
+        },
+      }}
+    >
+      <CardMedia
+        component="img"
+        height="270"
+        width="270"
+        image={image}
+        alt={name}
+        sx={{ backgroundColor: "black" }}
+      />
+      <CardActions>
+        <Typography variant="h6" component="div" align="left">
+          {name}
+        </Typography>
+      </CardActions>
+    </Card>
+  );
+
+  if (href != null) {
+    return (
+      <a href={href} style={{ textDecoration: "none" }}>
+        {content}
+      </a>
+    );
+  }
+
   return (
     <Link to={route} style={{ textDecoration: "none" }}>
-      <Card
-        square={true}
-        sx={{
-          ":hover": {
-            boxShadow: 20,
-          },
-        }}
-      >
-        <CardMedia
-          component="img"
-          height="270"
-          width="270"
-          image={image}
-          alt={name}
-          sx={{ backgroundColor: "black" }}
-        />
-        <CardActions>
-          <Typography variant="h6" component="div" align="left">
-            {name}
-          </Typography>
-        </CardActions>
-      </Card>
+      {content}
     </Link>
   );
 };
