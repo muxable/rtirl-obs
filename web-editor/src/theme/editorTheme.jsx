b0fbf9a9a24737af9b003130587cb77a41c33b10
Juan <juaoose@users.noreply.github.com>
2025-02-24 15:48:54 -0500

    chore: update scrollbar so it looks less like a buggy element
    Last time I opened the editor i thought it was something else but not a
    scroll bar...

--- web-editor/src/theme/editorTheme.jsx
+++ web-editor/src/theme/editorTheme.jsx
@@ -4,11 +4,12 @@ import OskariSemiBold from "../fonts/OskariG2-SemiBold.ttf";
 
 export const scrollbarStyles = {
   "&::-webkit-scrollbar": {
-    width: "0.5em",
+    width: "0.4em",
   },
   "&::-webkit-scrollbar-track": {
     boxShadow: "inset 0 0 6px rgba(0,0,0,0.00)",
     webkitBoxShadow: "inset 0 0 6px rgba(0,0,0,0.00)",
+    margin: "0 1em 0 1em",
   },
   "&::-webkit-scrollbar-thumb": {
     backgroundColor: "rgba(0,0,0,.1)",
